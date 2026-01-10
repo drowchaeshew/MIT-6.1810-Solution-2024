0daa24cc7c95044d236d9e443226f285bca24d1f
smsqo <15001243870@163.com>
2026-01-10 18:45:21 +0800

    Add Resource of the experiment

---
+++ kernel/mmap.c
@@ -0,0 +1,259 @@
+#include "types.h"
+#include "riscv.h"
+#include "defs.h"
+#include "param.h"
+#include "spinlock.h"
+#include "proc.h"
+#include "fs.h"
+#include "sleeplock.h"
+#include "file.h"
+#include "fcntl.h"
+#include "memlayout.h"
+
+#define min(a, b) ((a) <= (b) ? (a): (b))
+
+static struct vma *valloc(int sz);
+static struct vma *vfind(uint64 va);
+static int vdump(uint64 va, struct inode *ip, int off, int sz);
+
+// void *mmap(void *, uint64, int, int, int, int);
+uint64
+sys_mmap(void)
+{
+  int sz;
+  int prot;
+  int flag;
+  struct file *file;
+
+  // The 0th argument is always 0. No need to get it.
+  argint(1, &sz);
+  argint(2, &prot); // PROT_READ, PROT_WRITE or both
+  argint(3, &flag); // MAP_PRIVATE, or MAP_SHARED. (No need to share mem on MAP_SHARED)
+  argfd(4, 0, &file);
+  // The 5th argument is always 0. No need to get it. (File offset. Maybe of use later)
+
+  if ((prot & PROT_READ && !file->readable)
+    || ((prot & PROT_WRITE) && (flag & MAP_SHARED) && !(file->writable))
+  ) return -1;
+
+  struct vma *vp;
+
+  if ((vp = valloc(sz)) == 0)
+    panic("vget: No free vma");
+
+  filedup(file);
+  vp->file = file;
+  vp->prot = prot;
+  vp->flag = flag;
+  vp->off  = 0;
+  vp->loaded = 0;
+
+  return vp->start;
+}
+
+uint64
+sys_munmap(void)
+{
+  // Assumption:
+  // * vp->start & vp->end are PGSIZE aligned
+  uint64 start;
+  int sz; 
+
+  argaddr(0, &start);
+  argint(1, &sz);
+
+  struct vma *vp = vfind(start); 
+  if (!vp) {
+    return -1;
+  }
+
+  munmap(vp, start, start + sz);
+  return 0;
+}
+
+void
+munmap(struct vma *vp, uint64 start, uint64 end)
+{
+  // Assumption:
+  // * vp->start, vp->end, start, end are PGSIZE aligned
+  if (!vp->loaded)
+    return;  
+
+  end = min(end, vp->end);
+  if (start != vp->start && end != vp->end)
+    panic("You said no hole!");
+
+  pagetable_t pgtbl = myproc()->pagetable;
+  struct inode *ip = vp->file->ip;
+
+  for (uint64 addr = start; addr < end; addr += PGSIZE) {
+    if (vp->flag & MAP_SHARED) {
+      // What if failed? (res < 0)? We don't care about it.
+      vdump(addr, ip, addr - vp->start + vp->off, PGSIZE);
+    }
+    uvmunmap(pgtbl, addr, 1, 1);
+  }
+
+  if (vp->start == start && vp->end == end) {
+    vp->valid = 0;
+  } 
+  
+  // Adjust the vma range
+  if (vp->start == start) {
+    vp->start = end;
+    vp->off   = end - start;
+  } else if (vp->end == end) {
+    vp->end = start;
+  }
+}
+
+// Find a free vma and return it.
+static struct vma *
+valloc(int sz)
+{
+  struct vma *vp;
+  struct proc *proc = myproc();
+
+  // Since no other cpu will be handling this process,
+  // and no kerneltrap will be manipulating vma, 
+  // The lock is not required.
+  // (Really?)
+  for (vp = &proc->vma[0]; vp < &proc->vma[NOVMA]; vp++) {
+    if (!vp->valid) {
+      vp->valid = 1;
+      break;
+    }
+  }
+
+  if (vp == &proc->vma[NOVMA]) {
+    return 0; // no free vma slot
+  }
+
+  // As for where to map the file content, 
+  // Let's do some calculation: 
+  // 1. Each mmaped file can take at most 67 pages:
+  //    ans = MAXFILE * BSIZE / PGSIZE
+  // 2. Only 16 files can be opened:
+  //    ans = NOFILE
+  //
+  // We can map the nth file (in proc->ofile) to 
+  // MMAPBASE + n * 0x80000
+  // 
+  // Explanation: 0x80000 = 128 * PGSIZE > 67 * PGSIZE
+  // MMAPBASE + NOFILE * 0x80000 = 0x90800000, 
+  // still far away from VMMAX.
+  // Good solution. Mipa~
+
+  vp->start = MMAP(vp - &proc->vma[0]);
+  vp->end = vp->start + sz;
+
+  return vp;
+}
+
+static struct vma *
+vfind(uint64 va)
+{
+  struct vma *vp;
+  struct vma *arr = myproc()->vma;
+
+  for (vp = arr; vp < &arr[NOVMA]; vp++) {
+    if (!vp->valid)
+      continue;
+    if (va >= vp->start && va < vp->end) {
+      return vp;
+    }
+  }
+  return 0;
+}
+
+// return written sz
+static int 
+vdump(uint64 va, struct inode *ip, int off, int sz)
+{
+  int ret;
+  begin_op();
+  ilock(ip); // Lock here: ensure ip->size won't change
+  if (off > ip->size) {
+    ret = 0;
+    goto end;
+  }
+  sz = min(sz, ip->size - off);
+  if (writei(ip, 1, va, off, sz) < 0) {
+    ret = -1;
+    goto end;
+  }
+end:
+  iunlock(ip);
+  end_op();
+  return ret;
+}
+
+int 
+vload(uint64 va)
+{
+  // Simple achieve: 
+  // Load the whole file into mem at once,
+  // and set vp->loaded = 1 (used in munmap)
+  //
+  // Improve: load by page.
+
+  pagetable_t pgtbl = myproc()->pagetable;
+
+  struct vma *vp = vfind(va);
+  if (vp == 0 || vp->loaded) {
+    return -1;
+  }
+  struct inode *ip = vp->file->ip;
+
+  // No need to consider PTE_X.
+  int perm = PTE_U;
+  if (vp->prot & PROT_WRITE) perm |= PTE_W;
+  if (vp->prot & PROT_READ)  perm |= PTE_R;
+
+  // Assumption:
+  // vp->start is PGSIZE aligned
+
+  // Write something to the page. 
+  // !! Always rember to free mem on failure !!
+
+  char *mem;
+  uint64 addr;
+  for (addr = vp->start; addr < vp->end; addr += PGSIZE) {
+    if ((mem = kalloc()) == 0) {
+      goto bad_kalloc;
+    }
+    int offset = addr - vp->start + vp->off;
+
+    memset(mem, 0, PGSIZE);
+    ilock(ip);
+    if (readi(ip, 0, (uint64)mem, offset, PGSIZE) < 0) {
+      iunlock(ip);
+      goto bad;
+    }
+    iunlock(ip);
+
+    if (mappages(pgtbl, addr, PGSIZE, (uint64) mem, perm) != 0) 
+      goto bad;
+  }
+  vp->loaded = 1;
+  return 0;
+
+bad:
+  kfree(mem);
+bad_kalloc:
+  uvmunmap(pgtbl, vp->start, (addr - vp->start) / PGSIZE, 1);
+  return -1;
+}
+
+void
+vcopy(struct vma *dst, const struct vma *src)
+{
+  dst->valid = 1; 
+  dst->file = src->file;
+  dst->prot = src->prot;
+  dst->flag = src->flag;
+  dst->start = src->start;
+  dst->end = src->end;
+
+  dst->loaded = 0;
+}
